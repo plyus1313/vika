a52bbde07967affda5f256b43a253f90a0679953
Viktoriya Savaryn <37408680+plyus1313@users.noreply.github.com>
2018-06-13 18:57:51 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
 
 
 class App extends Component {
@@ -9,41 +9,90 @@ class App extends Component {
     this.state={
       data:[
         {
-          id: '1',
-          recipeId: '61922560-9279-432a-8608-fc79f674850d',
-          name: 'Shaped Pasta',
-          description: 'Shaped pastas are available in many different sizes and specific shapes. They include shapes that resemble shells, bow ties, spirals, snails, wheels and radiators. Shaped pastas are generally found dried. The smaller varieties work well with a simple sauce but most shaped pastas can be paired with a chunkier sauce because they are sturdy enough to hold up with the other ingredients. They are also used in pasta salads and casseroles.',
-          creation: Date(),
-          image: 'http://www.betterphoto.com/uploads/processed/1201/1201061143531img_0381.jpg'
-        },
-        {
-          id: '2',
-          recipeId: 'a5759254-ac5a-4ba8-a575-53b864440861',
-          name: 'Tubular Pasta',
-          description: 'Tubular pastas are any pastas that are in the shape of a tube. They are available in many different sizes and shapes. Some tubes are long and narrow while others are short and wide. They are found with smooth or grooved exteriors. They are often served with a heavy sauce, but are also used in salads and casseroles. ',
-          creation: Date(),
-          image: 'https://thumbs.dreamstime.com/b/looking-down-plate-cooked-tubular-pasta-seasoning-herbs-top-view-flavored-macaroni-noodles-served-green-parsley-33781552.jpg'
+          // id: '1',
+          recipeId: '',
+          name: '',
+          description: '',
+          // creation: Date(),
+          image: ''
         }
-      ]
+      ],
+    recipes: [],
+    currentView: 'list'
     }
   }
 
+  componentDidMount() {
+    fetch('https://cookbook-ataxztjiqa.now.sh/api/recipes')
+      .then(response => response.json())
+      .then(data => this.setState({recipes: data}));
+  }
+
+  onLoadDetailPage= (detailData) => {
+      this.setState(
+        {
+          data: {
+                    id: detailData.id,
+                    recipeId: detail.recipeId,
+                    name: detailData.name,
+                    description: detailData.description,
+                    creation: detailData.creation,
+                    image: detailData.image
+                  }
+                },
+                     currentView: 'detail');
+                }
+
+  resetView = () => {
+    this.setState({currentView: 'list'});
+  }
+
   render() {
+    const detailData = this.setState.data;
+
     return (
       <div>
          {
-           this.state.data.map((dynamicData, i) =>
-           <div>
-             {dynamicData.id} <br />
-             {dynamicData.name} <br />
-             {dynamicData.image}<br />
-           </div>
+      (this.state.currentView === 'list')
+      ?
+           this.state.recipes.map((dynamicData, i) => {
+         return (
+           <div className="br3 dim pa3 ma3 ba b--black-10 mv4 w-70 dib mw6 shadow-3 pointer">
+                {/* <br/> {dynamicData.id} <br /> */}
+                {dynamicData.recipeId} <br />
+                <br/><span className='title underline'>{dynamicData.name}</span> <br />
+                <br/><img src={dynamicData.image} width="200px" height="auto"/><br />
+                <br/>{ `${dynamicData.description.substring(0,200)} ... `} <br />
+                <br/>{dynamicData.creation} <br />
+                <br/>
+                  <button onClick={this.onLoadDetailPage(dynamicData)}>
+                      Details
+                  </button>
+                <br />
+            </div>
+                );
+              }
+              :
+               return (
+                 <div>
+                    <br/> {detailData.id} <br />
+                    <br/>{detailData.recipeId} <br />
+                    <br/>{detailData.name} <br />
+                    <br/><img src={detailData.image} width="150px" height="auto"/><br />
+                    <br/>{detailData.description} <br />
+                    <br/>{detailData.creation} <br />
+                    <br/>
+                        <button onClick={this.resetView()}>
+                              Back
+                        </button>
+                    <br />
+                  </div>
+                      }
+                    );
+                  }
+               </div>
+             );
+          }
+        }
 
-            )
-         }
-      </div>
-    );
-  }
-}
-
-export default App;
+        export default App;
